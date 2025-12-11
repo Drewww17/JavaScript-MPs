caf122c17422007d1f5e838b012240bdc6020fe8
Philip Andrew Luminarias <153975608+Drewww17@users.noreply.github.com>
2025-12-12 00:55:20 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 function replaceWord() {
-
-  const sentence = document.getElementById("sentence").value;
+  
+  const sentence  = document.getElementById("sentence").value;
   const oldWord   = document.getElementById("old-word").value;
   const newWord   = document.getElementById("new-word").value;
   const outputBox = document.getElementById("replace-output");
 
-  const regex = new RegExp(oldWord, "g");
+  const regex  = new RegExp(oldWord, "g");
   const result = sentence.replace(regex, newWord);
 
   outputBox.value = result;
 }
+
