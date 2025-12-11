6d93dd68288f79f32a9b6347c6fcc5ea55e93e7d
Philip Andrew Luminarias <153975608+Drewww17@users.noreply.github.com>
2025-12-12 00:46:47 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function replaceWord() {
-
     const sentence = document.getElementById("sentence").value;
     const oldWord = document.getElementById("old-word").value;
     const newWord = document.getElementById("new-word").value;
