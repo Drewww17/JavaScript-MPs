820ebc0b62cac01eb8e8fba208edf0e9a2e8387c
Philip Andrew Luminarias <153975608+Drewww17@users.noreply.github.com>
2025-12-12 09:34:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function replaceWord() {
-    const sentence = document.getElementById("sentence").value;
-    const oldWord = document.getElementById("old-word").value;
-    const newWord = document.getElementById("new-word").value;
-    const outputBox = document.getElementById("replace-output");
+  const sentence  = document.getElementById("sentence").value;
+  const oldWord   = document.getElementById("old-word").value;
+  const newWord   = document.getElementById("new-word").value;
+  const outputBox = document.getElementById("replace-output");
 
-    const result = sentence.replace(oldWord, newWord);
+  const result = sentence.replace(oldWord, newWord);
 
-    outputBox.value = result;
+  outputBox.value = result;
 }
-
