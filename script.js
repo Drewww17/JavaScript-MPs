43372cd6152f1f9b6bbc351000d922af21824b9a
Philip Andrew Luminarias <153975608+Drewww17@users.noreply.github.com>
2025-12-12 09:26:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function replaceWord() {
-  const sentence  = document.getElementById("sentence").value;
-  const oldWord   = document.getElementById("old-word").value;
-  const newWord   = document.getElementById("new-word").value;
-  const outputBox = document.getElementById("replace-output");
+    const sentence = document.getElementById("sentence").value;
+    const oldWord = document.getElementById("old-word").value;
+    const newWord = document.getElementById("new-word").value;
+    const outputBox = document.getElementById("replace-output");
 
-  const regex  = new RegExp(oldWord, "g");
-  const result = sentence.replace(regex, newWord);
+    const result = sentence.replace(oldWord, newWord);
 
-  outputBox.value = result;
+    outputBox.value = result;
 }
+
